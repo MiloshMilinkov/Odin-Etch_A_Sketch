ac26b0455de858b94ef812d0534fe689112df9d6
Milos Milinkov <milos300399@gmail.com>
2022-06-04 13:14:32 +0200

    Project skeleton.

---
+++ JS/app.js
@@ -0,0 +1,40 @@
+const DEFAULT_SIZE = 16;
+
+const container = document.querySelector(".gridContainer");
+const buttons = document.querySelectorAll(".btnSize");
+
+function createGrid(rows, cols) {
+    container.style.gridTemplateColumns = `repeat(${rows}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${cols}, 1fr)`
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-item";
+    };
+};
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.id == "btnSize1") {
+            reloadGrid();
+            createGrid(16, 16);
+        } else if (button.id == "btnSize2") {
+            reloadGrid();
+            createGrid(32, 32);
+        } else if (button.id == "btnSize3") {
+            reloadGrid();
+            createGrid(64, 64);
+        }
+    })
+})
+
+function clearGrid() {
+    container.innerHTML = '';
+}
+
+function reloadGrid() {
+    clearGrid()
+
+}
+window.onload = () => {
+    createGrid(DEFAULT_SIZE, DEFAULT_SIZE)
+}
